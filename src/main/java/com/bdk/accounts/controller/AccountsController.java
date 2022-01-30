874603f7717d5a7ea107e4adaa7b5e0d72407d2e
Britto116 <britto116@gmail.com>
2022-01-30 17:22:51 +0530

    accounts first commit

--- src/main/java/com/bdk/accounts/controller/AccountsController.java
+++ src/main/java/com/bdk/accounts/controller/AccountsController.java
@@ -1,18 +1,22 @@
 package com.bdk.accounts.controller;
 
 import com.bdk.accounts.config.AccountsServiceConfig;
-import com.bdk.accounts.model.Accounts;
-import com.bdk.accounts.model.Customer;
-import com.bdk.accounts.model.Properties;
+import com.bdk.accounts.model.*;
 import com.bdk.accounts.repository.AccountsRepository;
+import com.bdk.accounts.service.client.CardsFeignClient;
+import com.bdk.accounts.service.client.LoansFeignClient;
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.ObjectWriter;
+import io.github.resilience4j.circuitbreaker.annotation.CircuitBreaker;
+import io.github.resilience4j.ratelimiter.annotation.RateLimiter;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class AccountsController {
@@ -24,6 +28,11 @@ public class AccountsController {
     @Autowired
     AccountsServiceConfig accountsConfig;
 
+    @Autowired
+    LoansFeignClient loansFeignClient;
+
+    @Autowired
+    CardsFeignClient cardsFeignClient;
 
     @PostMapping("/myAccount")
     public Accounts getAccountDetails(@RequestBody Customer customer) {
@@ -44,5 +53,40 @@ public class AccountsController {
                 accountsConfig.getMailDetails(), accountsConfig.getActiveBranches());
         String jsonStr = ow.writeValueAsString(properties);
         return jsonStr;
+    }
+    @PostMapping("/myCustomerDetails")
+    @CircuitBreaker(name="detailsForCustomerSupportApp",fallbackMethod = "myCustomerDetailsFallBack")
+    public CustomerDetails myCustomerDetails(@RequestBody Customer customer) {
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        List<Cards> cards = cardsFeignClient.getCardDetails(customer);
+
+
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        customerDetails.setCards(cards);
+
+        return customerDetails;
+
+    }
+    private CustomerDetails myCustomerDetailsFallBack(Customer customer, Throwable t) {
+        Accounts accounts = accountsRepository.findByCustomerId(customer.getCustomerId());
+        List<Loans> loans = loansFeignClient.getLoansDetails(customer);
+        CustomerDetails customerDetails = new CustomerDetails();
+        customerDetails.setAccounts(accounts);
+        customerDetails.setLoans(loans);
+        return customerDetails;
+
+    }
+
+    @GetMapping("/sayHello")
+    @RateLimiter(name = "sayHello", fallbackMethod = "sayHelloFallback")
+    public String sayHello() {
+        return "Hello, Welcome to brittodkbank";
+    }
+
+    private String sayHelloFallback(Throwable t) {
+        return "Hi, Welcome to EazyBank";
     }
 }
